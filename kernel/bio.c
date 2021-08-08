ed8195e2e8e7879bed27defb2abdee8b682d6bf3
余振波 <yuzhenbo@yuzhenbodeMacBook-Pro.local>
2021-08-09 07:53:33 +0800

    bcache 未完成

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,69 +23,106 @@
 #include "fs.h"
 #include "buf.h"
 
+extern uint ticks;
+#define HTSIZE 13
 struct {
-  struct spinlock lock;
-  struct buf buf[NBUF];
+  struct spinlock lock[HTSIZE];
+  struct buf buf[HTSIZE];
 
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf head;
+  //struct buf head;
+  int info[HTSIZE];
+  
 } bcache;
+
+void findHashTable(uint blockno){
+	uint position = blockno%HTSIZE;
+}
 
 void
 binit(void)
 {
   struct buf *b;
 
-  initlock(&bcache.lock, "bcache");
+  for (int i=0; i<HTSIZE; i++){
+	  initlock(&bcache.lock, "bcache");
+  }
 
   // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+  //bcache.head.prev = &bcache.head;
+  //bcache.head.next = &bcache.head;
+  for(b = bcache.buf; b < bcache.buf+HTSIZE; b++){
+   // b->next = bcache.head.next;
+   // b->prev = &bcache.head;
+   // initsleeplock(&b->lock, "buffer");
+   // bcache.head.next->prev = b;
+   // bcache.head.next = b;
+   b->lastestTime = 0;
   }
 }
 
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
-static struct buf*
-bget(uint dev, uint blockno)
-{
-  struct buf *b;
+//static struct buf*
+//bget(uint dev, uint blockno)
+//{
+//  struct buf *b;
+//
+//  acquire(&bcache.lock);
+//
+//  // Is the block already cached?
+//  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+//    if(b->dev == dev && b->blockno == blockno){
+//      b->refcnt++;
+//      release(&bcache.lock);
+//      acquiresleep(&b->lock);
+//      return b;
+//    }
+//  }
+//
+//  // Not cached.
+//  // Recycle the least recently used (LRU) unused buffer.
+//  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
+//    if(b->refcnt == 0) {
+//      b->dev = dev;
+//      b->blockno = blockno;
+//      b->valid = 0;
+//      b->refcnt = 1;
+//      release(&bcache.lock);
+//      acquiresleep(&b->lock);
+//      return b;
+//    }
+//  }
+//  panic("bget: no buffers");
+//}
+int bget(int dev, uint blockno){
+	int position = blockno % HTSIZE;
+	int oldpos = position;
 
-  acquire(&bcache.lock);
-
-  // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
-    if(b->dev == dev && b->blockno == blockno){
-      b->refcnt++;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
-    }
-  }
-
-  // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
-    }
-  }
-  panic("bget: no buffers");
+	// 将已经在哈希表中的buf找出, 否则找出最近最少被使用的
+	while (bcache.info[position] == 1){
+		if (bcache.buf[position].blockno == blockno){
+			return position;	
+		}else{
+			position++;	
+			if (position >= HTSIZE)
+				position -= HTSIZE;
+			if (position == oldpos){
+				uint min = UINT_MAX;
+				for (int i=position; i<position+HTSIZE-1; i++){
+					if (bcache.buf[i%HTSIZE].lastestTime < min){
+						min = bcache.info[i%HTSIZE];	
+						position = i%HTSIZE;
+					}	
+				}
+				return position;
+			}
+		}	
+	}
+	return position;
 }
 
 // Return a locked buf with the contents of the indicated block.
